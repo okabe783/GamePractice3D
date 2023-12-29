17c329a2072a211e48e8421b0498c2d86ce72264
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-12-29 12:17:11 +0900

    [Add]インプットシステムの作成

---
+++ Assets/Script/AnimatorController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    // void Update()
+    // {
+    //     if (animator != null)
+    //     {
+    //         animator.SetBool("OnGround", isOnGround);
+    //     }
+    // }
+}
