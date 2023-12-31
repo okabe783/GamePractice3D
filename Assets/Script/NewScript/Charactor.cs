70e0c7e8e39b375439eab50c2809e3ef23ae7558
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-12-31 14:41:31 +0900

    [Move]長押しでジャンプが少し長くなるようにした

--- Assets/Script/NewScript/Charactor.cs
+++ Assets/Script/NewScript/Charactor.cs
@@ -12,8 +12,13 @@ public class Charactor : MonoBehaviour
     private CapsuleCollider capsuleCollider;
 
     public float jumpSpeed = 10;
+    public float jumpGravityScale = 0.6f;
     public float groundDistance = 0.01f;
+    
     private bool isGround = false;
+    private bool isJumping = false;
+    //地面として認識しないコライダーの設定
+    public LayerMask groundMask = ~0;
 
     private void Start()
     {
@@ -36,7 +41,12 @@ public class Charactor : MonoBehaviour
         if (state & isGround)
         {
             rb.velocity += Vector3.up * jumpSpeed;
-            isGround = true;
+            isJumping = true;
+        }
+
+        if (!state)
+        {
+            isJumping = false;
         }
     }
 
@@ -47,18 +57,29 @@ public class Charactor : MonoBehaviour
 
     bool CheckForGround()
     {
-        //capsuleBottomはオブジェクトに対して相対的な足元の位置からfeetPosition(絶対的な位置)に変換
+        //Charactorのコライダーから下まで(中心から下向きベクトルを高さの半分だけかける)の計算
         Vector3 capsuleBottom = capsuleCollider.center + Vector3.down * capsuleCollider.height * 0.5f;
+        //charactorの足元の位置を計算。カプセルの下をワールド座標に変換して上向きのベクトルを地面の距離だけかける
         Vector3 feetPosition = transform.TransformPoint(capsuleBottom)　+ Vector3.up * groundDistance;
 
         //Raycastは足元よりも少し上の位置から始める必要がある
-        bool raycastHit = Physics.Raycast(feetPosition, Vector3.down, groundDistance * 2f);
+        bool raycastHit = Physics.Raycast(feetPosition, Vector3.down, groundDistance * 2f,groundMask);
         
         return raycastHit;
     }
 
+    // fixedUpdateはjumpしている場合、重力と反比例の力を加える。なのでrigidbodyの重量を忘れずに計算。  
     private void FixedUpdate()
     {
         isGround = CheckForGround();
+        if (rb.velocity.y < 0)
+        {
+            isJumping = false;
+        }
+        //jumpの高さを調節
+        if (isJumping)
+        {
+            rb.AddForce(Physics.gravity * rb.mass * (jumpGravityScale - 1f));
+        }
     }
 }
